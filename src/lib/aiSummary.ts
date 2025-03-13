137d44bb30852ffd89bdfe3f1efd83011060a965
Musaddiq101 <mbhutto34@gmail.com>
2025-03-14 07:12:54 +0800

    maxDuration update

--- src/lib/aiSummary.ts
+++ src/lib/aiSummary.ts
@@ -10,14 +10,12 @@ import pLimit from 'p-limit';
 
 const limit = pLimit(1);
 
-
-
 const vertexAi = new VertexAI({
     project:  process.env.GOOGLE_PROJECT_ID as string,
     location: process.env.LOCATION_GOOGLE as string,
     
-    
 });
+
 
 
 const PROJECT_ID = process.env.GOOGLE_PROJECT_ID; // Your GCP Project ID
